2a798315cf45955fb4c1c1d1c0cfb0b3ffca3048
lijougeorge <lijougeorge@gmail.com>
2025-04-21 17:58:25 +0530

    Initialize empty main branch

--- modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ids" {
-  description = "IDs of the EC2 instances"
-  value       = aws_instance.linux_server[*].id
-}
-
-output "instance_private_ips" {
-  description = "The private IP addresses of the EC2 instances"
-  value       = aws_instance.linux_server[*].private_ip
-}
--- modules/nlb-alb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID for the EFS"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs for EKS"
-  type        = list(string)
-}
